1d2aea9c415817661b6bda5caa7fe85b04823ea1
Aleksey <hydrombee@gmail.com>
2022-01-13 09:19:43 +0300

    change babel to .js and added refresh plug in dev

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+const plugins = []
+/* массив плагинов, которые будет использовать babel */
+
+if (process.env.NODE_ENV !== "production") {
+  plugins.push("react-refresh/babel")
+}
+/* если режим разработки не является "production", тогда к плагинам добавится плагин react-refresh -
+плагин быстрой перезагрузки без обновления всей страницы */
+
+module.exports = {
+    presets: [
+      "@babel/preset-env",
+      /*
+      @babel/preset-env говорит webpack скопилировать весь синтаксис в ES5(который понимают браузеры)
+      */
+      ["@babel/preset-react", {runtime: "automatic" }],
+      /*
+      @babel/preset-react добавляет поддержку jsx синтаксиса (реакт)
+      время выполнения "automatic" импортирует функции, которые транспилирует jsx */
+    ],
+   
+      plugins: plugins,
+  }
